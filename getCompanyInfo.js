e0eb3e9774c4a9a1caed1793a4c989f18250a4b5
muzykka <mmuzzykka@gmail.com>
2024-06-22 15:08:13 +0300

    added 9.2 big boss task

---
+++ getCompanyInfo.js
@@ -0,0 +1,68 @@
+const company = {
+    name: 'Велика Компанія',
+    type: 'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+      {
+        name: 'Клієнт 1',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків',
+        partners: [
+          {
+            name: 'Клієнт 1.1',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+          },
+          {
+            name: 'Клієнт 1.2',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+              {
+                name: 'Клієнт 1.2.3',
+                type: 'subSubCompany',
+                uses: 'Рішення для продажу квитків',
+                sells: 'Рішення для продажу квитків',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Клієнт 2',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків'
+      }
+    ]
+  };
+  
+function findValueByKey(obj, companyName) {
+    if (companyName === obj.name) {
+        return obj
+    }
+    if (obj.clients) {
+        for (let client of obj.clients) {
+            let result = findValueByKey(client, companyName);
+            if (result) {
+                return result;
+            }
+        }
+    }
+    if (obj.partners) {
+        for (let partner of obj.partners) {
+            let result = findValueByKey(partner, companyName);
+            if (result) {
+                return result;
+            }
+        }
+    }
+    return null;
+}
+
+console.log(findValueByKey(company, "Велика Компанія"))
+  
